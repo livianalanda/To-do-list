a556b0d678ee0222e8a7466d1e4a20e24c12afbd
Lívia Nalanda <livianalanda16@gmail.com>
2025-04-23 13:20:12 -0300

    Adicionado funcionalidade do calendario script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const button = document.querySelector('.button-add-task')
 const input = document.querySelector('.input-task')
 const listaCompleta = document.querySelector('.list-tasks')
-
+const calendarioCorpo = document.getElementById('calendarioCorpo');
+const mesAno = document.getElementById('mesAno');
+const bntMesAnterior = document.getElementById('mesAnterior');
+const bntMesProximo = document.getElementById('mesProximo');
 
 let minhaListaDeItens = []
+let dataAtual = new Date ();
 
 function adicionarNovaTarefa() {
     minhaListaDeItens.push({
@@ -59,6 +63,44 @@ function recarregarTarefas() {
 
     mostrarTarefas()
 }
+
+function mostrarAba (id) {
+    const abas = document.querySelectorAll('.aba');
+    abas.forEach(aba => aba.style.display = 'none');
+    document.getElementById(id).style.display = 'block';
+}
+
+function renderizarCalendario(data) {
+    const ano = data.getFullYear();
+    const mes = data.getMonth();
+
+    const primeiroDia = new Date(ano, mes + 1);
+    const ultimoDia = new Date(ano, mes + 1, 0);
+    const primeiroDiaSemana = primeiroDia.getDay();
+
+    mesAno.textContent = data.toLocaleDateString('pt-BR', { month: 'long', year: 'numeric' });
+
+    calendarioCorpo.innerHTML = '';
+
+    for (let i = 0; i < primeiroDiaSemana; i++) {
+        calendarioCorpo.innerHTML += '<div></div>';
+    }
+
+    for (let dia = 1; dia <= ultimoDia.getDate(); dia++)  {
+        calendarioCorpo.innerHTML += `<div>${dia}</div>`;
+    }
+}
+
+bntMesAnterior.addEventListener('click', () => {
+    dataAtual.setMonth(dataAtual.getMonth() - 1);
+    renderizarCalendario(dataAtual);
+});
+
+bntMesProximo.addEventListener('click', () => {
+    dataAtual.setMonth(dataAtual.getMonth() + 1);
+    renderizarCalendario(dataAtual);
+});
+
 
 recarregarTarefas()
 button.addEventListener('click', adicionarNovaTarefa)
